22f3f0168bd430f68f074cd5db9fd49e62a8d153
clarkenciel <clarkenciel@gmail.com>
2019-12-11 18:58:15 -0800

    three part one done

--- src/three.rs
+++ src/three.rs
@@ -1,11 +1,14 @@
+use std::collections::HashSet;
 use std::error::Error;
 use std::fmt;
-use std::ops::Add;
+use std::hash::Hash;
+use std::iter;
 use std::str::FromStr;
 
 use crate::challenge;
+use crate::input;
 
-struct Challenge {}
+pub struct Challenge {}
 
 impl challenge::Challenge for Challenge {
     fn run(&mut self, part: &str) -> challenge::ChallengeResult {
@@ -20,16 +23,18 @@ impl challenge::Challenge for Challenge {
 }
 
 impl Challenge {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self {}
     }
 
     fn run_one(&self) -> challenge::ChallengeResult {
-        Ok(())
+        input::string("3", "1.txt")
+            .map_err(|e| input::read_error("3", "1", "1.txt", e))
+            .map(|s| println!("{}", calculate(&*s)))
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
 enum Direction {
     U,
     R,
@@ -61,14 +66,12 @@ impl fmt::Display for ParseDirectionError {
     }
 }
 
-#[derive(Debug, Eq, PartialEq)]
-struct Motion(i64, i64);
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
+struct Motion(Direction, u64);
 
-impl Add for &Motion {
-    type Output = Motion;
-
-    fn add(self, other: Self) -> Self::Output {
-        Motion(self.0 + other.0, self.1 + other.1)
+impl Motion {
+    fn expand(&self) -> impl Iterator<Item = Self> {
+        iter::repeat(Self(self.0, 1)).take(self.1 as usize)
     }
 }
 
@@ -81,14 +84,9 @@ impl FromStr for Motion {
         str::parse::<Direction>(dir)
             .map_err(|e| ParseMotionError(s.to_owned(), Box::new(e)))
             .and_then(|d| {
-                str::parse::<i64>(rest)
+                str::parse::<u64>(rest)
                     .map_err(|e| ParseMotionError(s.to_owned(), Box::new(e)))
-                    .and_then(|n| match d {
-                        Direction::U => Ok(Motion(n, 0)),
-                        Direction::R => Ok(Motion(0, n)),
-                        Direction::D => Ok(Motion(-n, 0)),
-                        Direction::L => Ok(Motion(0, -n)),
-                    })
+                    .map(|n| Motion(d, n))
             })
     }
 }
@@ -108,7 +106,7 @@ impl fmt::Display for ParseMotionError {
     }
 }
 
-#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone, Copy)]
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone, Copy, Hash)]
 struct Pos(i64, i64);
 
 impl Pos {
@@ -117,36 +115,53 @@ impl Pos {
     }
 
     fn mv(&self, mot: &Motion) -> Self {
-        Pos(self.0 + mot.0, self.1 + mot.1)
+        match mot.0 {
+            Direction::U => Self(self.0, self.1 + mot.1 as i64),
+            Direction::R => Self(self.0 + mot.1 as i64, self.1),
+            Direction::D => Self(self.0, self.1 - mot.1 as i64),
+            Direction::L => Self(self.0 - mot.1 as i64, self.1),
+        }
     }
 
     fn dist(&self, other: &Self) -> u64 {
-        ((other.0 - self.0) + (other.1 + self.1)).abs() as u64
+        ((other.0 - self.0).abs() + (other.1 - self.1).abs()) as u64
     }
 }
 
 fn calculate(input: &str) -> u64 {
     let parsed_wires = input
         .lines()
-        .map(|s| s.split(",").map(|ms| str::parse(ms)).collect())
+        .map(|s| s.split(",").map(|ms| ms.parse()).collect())
         .collect::<Result<Vec<Vec<Motion>>, ParseMotionError>>();
+
     match parsed_wires {
         Err(e) => panic!("{}", e.description()),
         Ok(mots) => {
-            let [one, two] = mots.get(0..=1).unwrap();
-            let zero = Pos::origin();
+            let mots = mots.get(0..=1).expect("No instruction streams");
 
-            one.iter()
-                .zip(two.iter())
-                .scan((Pos::origin(), Pos::origin()), |(p1, p2), (m1, m2)| {
-                    Some((p1.mv(m1), p2.mv(m2)))
-                })
-                .filter_map(|(p1, p2)| if p1 == p1 { Some(p1) } else { None })
-                .min_by_key(|p| zero.dist(&p))
-                .unwrap()
-                .dist(&zero)
+            let one = positions(&mots[0]);
+            let two = positions(&mots[1]);
+
+            let zero = Pos::origin();
+            let intersect = one.intersection(&two);
+            intersect
+                .into_iter()
+                .map(|p| p.dist(&zero))
+                .min()
+                .expect("no minimum")
         }
     }
+}
+
+fn positions(motions: &Vec<Motion>) -> HashSet<Pos> {
+    motions
+        .iter()
+        .flat_map(Motion::expand)
+        .scan(Pos::origin(), |p, m| {
+            *p = p.mv(&m);
+            Some(*p)
+        })
+        .collect()
 }
 
 #[cfg(test)]
@@ -154,7 +169,13 @@ mod test {
     use super::*;
 
     #[test]
-    fn test() {
+    fn test_calculate() {
+        let input_str = "R8,U5,L5,D3
+U7,R6,D4,L4";
+        let expectation = 6;
+        let result = calculate(input_str);
+        assert_eq!(expectation, result);
+
         let input_str = "R75,D30,R83,U83,L12,D49,R71,U7,L72
 U62,R66,U55,R34,D71,R55,D58,R83";
         let expectation = 159;
@@ -166,5 +187,26 @@ U98,R91,D20,R16,D67,R40,U7,R15,U6,R7";
         let expectation = 135;
         let result = calculate(input_str);
         assert_eq!(expectation, result);
+    }
+
+    #[test]
+    fn test_motion_expand() {
+        let mot = Motion(Direction::U, 10);
+        let mots: Vec<Motion> = mot.expand().collect();
+        assert_eq!(
+            [
+                Motion(Direction::U, 1),
+                Motion(Direction::U, 1),
+                Motion(Direction::U, 1),
+                Motion(Direction::U, 1),
+                Motion(Direction::U, 1),
+                Motion(Direction::U, 1),
+                Motion(Direction::U, 1),
+                Motion(Direction::U, 1),
+                Motion(Direction::U, 1),
+                Motion(Direction::U, 1),
+            ],
+            mots[..]
+        );
     }
 }
